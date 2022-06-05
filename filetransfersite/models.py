b299dfe7247d6c12d379ed6a9efa44a5c1025b20
TheArchons <progamer9138@gmail.com>
2022-06-04 22:12:44 -0700

    prevent duplicate files of the same name

--- filetransfersite/models.py
+++ filetransfersite/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 class files(models.Model):
     title = models.CharField(max_length=100)
-    file = models.FileField(upload_to='files/')
+    fileName = models.CharField(max_length=100)
--- filetransfersite/views.py
+++ filetransfersite/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django import forms
 from io import BytesIO
 import os
+from os.path import exists
 
 directory_path = os.getcwd()
 
@@ -20,9 +21,12 @@ def upload(request):
         if form.is_valid():
             global directory_path
             file = form.cleaned_data['file']
-            with open(str(file), 'wb+') as destination:
-                for chunk in file.chunks():
-                    destination.write(chunk)
+            if not exists(str(file)):
+                with open(str(file), 'wb+') as destination:
+                    for chunk in file.chunks():
+                        destination.write(chunk)
+            else:
+                return render(request, 'error.html', {'error': 'Error: File already exists'})
             return render(request, 'index.html', {'url' : 'asd'})
         else:
             #print errors
